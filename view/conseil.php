fc5ede9812dd1cfc5b57931e13cb7cdba6ed1602
CRIDIP-SWD <mmockelyn@cridip.com>
2016-01-05 16:38:41 +0100

    CRIDIP

---
+++ view/conseil.php
@@ -0,0 +1,70 @@
+<!-- Page Title-->
+<section id="page-title" class="page-title-dark">
+
+    <div class="container clearfix">
+        <h1>CONSEIL</h1>
+        <ol class="breadcrumb">
+            <li><a href="#">SAS CRIDIP</a></li>
+            <li class="active">Conseil en entreprise</li>
+        </ol>
+    </div>
+
+</section>
+<!-- #page-title end -->
+<!-- Content-->
+<section id="content">
+
+    <div class="content-wrap">
+        <div class="promo promo-full promo-border header-stick bottommargin-lg">
+            <div class="container clearfix">
+                <h3>Appelez-nous aujourd'hui au <span>0899 492 648</span> <br>où écrivez-nous à <span>contact@cridip.com</span></h3>
+                <span>Nous nous efforçons de forunir à nos clients tout notre soutien pour mener leurs projets à bien.</span>
+                <a class="button button-reveal button-xlarge button-rounded tright" href="index.php?view=contact"><i class="icon-angle-right"></i><span>Contactez-nous</span></a>
+            </div>
+        </div>
+        <div class="container clearfix bottommargin-lg topmargin">
+            <div class="fancy-title title-dotted-border title-center">
+                <h1>Nous vous <span>Conseillons</span> dans la vie de votre entreprise</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="feature-box fbox-effect">
+                        <div class="fbox-icon">
+                            <a href="index.php?view=event"><i class="icon-circle-arrow-right i-alt"></i></a>
+                        </div>
+                        <h3>Solution Conseil</h3>
+                        <p>Nos équipes dédiés au conseil d'entreprise, vous permettrons de prendre les bonnes décisions au bon moment.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature-box fbox-effect">
+                        <div class="fbox-icon">
+                            <a href="index.php?view=event"><i class="icon-circle-arrow-right i-alt"></i></a>
+                        </div>
+                        <h3>Gestion</h3>
+                        <p>Des questions sur la gestion d'une entreprise, nos équipes répondrons à vos questions.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature-box fbox-effect">
+                        <div class="fbox-icon">
+                            <a href="index.php?view=event"><i class="icon-circle-arrow-right i-alt"></i></a>
+                        </div>
+                        <h3>Qualité RH</h3>
+                        <p>La Relation Humaine est le point d'encrage d'une société, nous vous aidrons à comprendre sons fonctionnement.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="promo promo-dark promo-full promo-uppercase bottommargin footer-stick">
+            <div class="container clearfix">
+                <h3>Appelez-nous aujourd'hui au <span>0899 492 648</span> <br>où écrivez-nous à <span>contact@cridip.com</span></h3>
+                <span>Nous nous efforçons de forunir à nos clients tout notre soutien pour mener leurs projets à bien.</span>
+                <a class="button button-reveal button-xlarge button-rounded tright" href="index.php?view=contact"><i class="icon-angle-right"></i><span>Contactez-nous</span></a>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- #content end -->
+
